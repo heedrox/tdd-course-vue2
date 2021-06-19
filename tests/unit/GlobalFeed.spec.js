2dddfb867b9ec4fd34b0b2c24d473c95d8e73170
jmarti <jmarti@theinit.com>
2021-06-19 19:31:40 +0200

    prepare first test

--- tests/unit/GlobalFeed.spec.js
+++ tests/unit/GlobalFeed.spec.js
@@ -2,9 +2,17 @@ import { mount } from '@vue/test-utils';
 import App from '@/App';
 
 describe('Global Feed', () => {
-  it('is shown', () => {
+  it('is shown with 0 posts', () => {
     const app = mount(App);
 
-    expect(app.findAll('.post-preview').length).toEqual(2);
+    expect(app.findAll('.post-preview').length).toEqual(0);
+  });
+
+  it.skip('is shown with 1 post', () => {
+
+    const app = mount(App);
+
+    expect(app.findAll('.post-preview').length).toEqual(0);
+
   });
 });
