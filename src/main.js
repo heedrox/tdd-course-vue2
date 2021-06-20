f6ad494028c2c168c85f41ab69acf0d0e5746fc0
jmarti <jmarti@theinit.com>
2021-06-20 10:50:52 +0200

    add base url

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import i18n from './i18n';
+import axios from 'axios';
 
 Vue.config.productionTip = false;
 
+axios.defaults.baseURL = 'https://conduit.productionready.io/api/';
 new Vue({
   render: h => h(App),
   router,
