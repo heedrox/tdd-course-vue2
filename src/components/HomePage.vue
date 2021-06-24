249b6e03e29798f9180598bc78272b81e0eb8f67
jmarti <jmarti@theinit.com>
2021-06-24 09:43:21 +0200

    show only 10 articles

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,38 +1,48 @@
 <template>
   <div class="home-page">
-<AppBanner/>
+    <AppBanner/>
 
-<div class="container page">
-  <div class="row">
+    <div class="container page">
+      <div class="row">
 
-    <div class="col-md-12">
-      <div v-if="state === 'LOADING'" data-testid="feed-loading">
-        {{ $t('feed.loading') }}
-      </div>
-      <div class="post-preview" data-testid="feed-post" v-for="article in articles" :key="article.slug">
-        <div class="post-meta">
-          <a href="profile.html"><img data-testid="author-image" :src="article.author.image"/></a>
-          <div class="info">
-            <a href="profile.html" class="author" data-testid="author-username">{{ article.author.username }}</a>
-            <span class="date" data-testid="post-date">{{ toPrettyDate(article.createdAt) }}</span>
+        <div class="col-md-12">
+          <div v-if="state === 'LOADING'" data-testid="feed-loading">
+            {{ $t('feed.loading') }}
           </div>
-          <button class="btn btn-outline-primary btn-sm pull-xs-right" data-testid="post-favorites">
-            <i class="ion-heart"></i> {{ article.favoritesCount }}
-          </button>
+          <div class="post-preview" data-testid="feed-post" v-for="article in articles" :key="article.slug">
+            <div class="post-meta">
+              <a href="profile.html"><img data-testid="author-image" :src="article.author.image"/></a>
+              <div class="info">
+                <a href="profile.html" class="author" data-testid="author-username">{{ article.author.username }}</a>
+                <span class="date" data-testid="post-date">{{ toPrettyDate(article.createdAt) }}</span>
+              </div>
+              <button class="btn btn-outline-primary btn-sm pull-xs-right" data-testid="post-favorites">
+                <i class="ion-heart"></i> {{ article.favoritesCount }}
+              </button>
+            </div>
+            <a class="preview-link" data-testid="post-link" @click="clickPost(article.slug)">
+              <h1 data-testid="post-title">
+                <i v-if="article.favoritesCount >= 30" title="More than 30 likes!" class="text-primary ion-star mx-1"
+                   data-testid="post-star"></i>
+                {{ article.title }}
+              </h1>
+              <p data-testid="post-description">{{ article.description }}</p>
+              <span data-testid="post-readmore">{{ $t('feed.post.read-more') }}</span>
+            </a>
+          </div>
+          <nav aria-label="Page navigation example" data-testid="feed-pagination" v-if="articlesCount > 10">
+            <ul class="pagination">
+              <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+              <li class="page-item" data-testid="page-number"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">Next</a></li>
+            </ul>
+          </nav>
         </div>
-        <a class="preview-link" data-testid="post-link" @click="clickPost(article.slug)">
-          <h1 data-testid="post-title">
-            <i v-if="article.favoritesCount >= 30" title="More than 30 likes!" class="text-primary ion-star mx-1" data-testid="post-star"></i>
-            {{ article.title }}
-          </h1>
-          <p data-testid="post-description">{{ article.description }}</p>
-          <span data-testid="post-readmore">{{ $t('feed.post.read-more') }}</span>
-        </a>
       </div>
     </div>
   </div>
-</div>
-</div>
 </template>
 <script>
 import AppBanner from '@/components/AppBanner';
@@ -47,12 +57,14 @@ export default {
   data() {
     return {
       state: 'LOADING',
-      articles: []
+      articles: [],
+      articlesCount: 0
     };
   },
   async mounted() {
-    const articles = await axios.get('/articles');
+    const articles = await axios.get('/articles', { params: { limit: 10 } } );
     this.articles = articles.data.articles;
+    this.articlesCount = articles.data.articlesCount;
     this.state = 'LOADED';
   },
   methods: {
---
+++ tests/unit/HomePage-FeedPagination.spec.js
@@ -0,0 +1,64 @@
+import { mount } from '@vue/test-utils';
+import flushPromises from 'flush-promises';
+import ONE_ARTICLE from '../data/articles/one-article.json';
+import TWO_ARTICLES from '../data/articles/two-articles.json';
+import HomePage from '@/components/HomePage';
+
+const PAGINATION_SELECTOR = '[data-testid=feed-pagination]';
+
+const changeSlug = (article, idx) => ({
+  ...article,
+  slug: `${article.slug}-${idx}`
+});
+
+const buildArticles = (numPageArticles, numArticlesCount) => {
+  const article = JSON.parse(JSON.stringify(ONE_ARTICLE.articles[0]));
+  return {
+    articles: Array(numPageArticles).fill(article).map(changeSlug),
+    articlesCount: numArticlesCount
+  };
+};
+
+describe('Home Page - Feeds pagination', () => {
+
+  it('asks for the first 10 posts first time', async () => {
+    mockAxios.onGet('/articles').reply(200, TWO_ARTICLES);
+
+    mount(HomePage);
+    await flushPromises();
+
+    expect(mockAxios.history.get[0].params.limit).toEqual(10);
+  });
+
+  it.skip.each`
+    numArticlesCount | 
+  `('shows number of pages', async () => {
+    const articles = buildArticles(10, 11);
+    mockAxios.onGet('/articles').reply(200, articles);
+
+    const app = mount(HomePage);
+    await flushPromises();
+
+    expect(app.findAll('[data-testid=page-number]').length).toEqual(1);
+  });
+
+  it('does not show pagination when <10 posts', async () => {
+    mockAxios.onGet('/articles').reply(200, TWO_ARTICLES);
+
+    const app = mount(HomePage);
+    await flushPromises();
+
+    expect(app.find(PAGINATION_SELECTOR).exists()).toBeFalsy();
+  });
+
+  it('shows pagination when >=10 posts', async () => {
+    const articles = buildArticles(10, 11);
+    mockAxios.onGet('/articles').reply(200, articles);
+
+    const app = mount(HomePage);
+    await flushPromises();
+
+    expect(app.find(PAGINATION_SELECTOR).exists()).toBeTruthy();
+  });
+
+});
